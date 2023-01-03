10f274974deeb40964c958b939077f402a8e9128
inderdeep01 <inderdeepsidhu01@gmail.com>
2023-01-03 22:20:31 +0530

    scope

---
+++ basics/scope.java
@@ -0,0 +1,12 @@
+class Scope{
+    public static void main(String args[]){
+        int x = 10;
+        if(x==10){
+            int y = 20;
+            System.out.println(x+" "+y);
+            x = y*2;
+        }
+        y = 100;
+        System.out.println(x+" "+y);
+    }
+}
