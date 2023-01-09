8ffddb90bb0a79fbe3def8f22f81d76691483b53
inderdeep01 <inderdeepsidhu01@gmail.com>
2023-01-09 22:23:06 +0530

    typecasting

---
+++ basics/casting.java
@@ -0,0 +1,10 @@
+class Casting{
+    public static void main(String args[]){
+        byte b = 50;
+        int a = b;
+        System.out.println(a);
+        int a2 = 50;
+        byte b2 = (byte)a2;
+        System.out.println(b2);
+    }
+}
